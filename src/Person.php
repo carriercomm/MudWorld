a51c90b0dbbbe6621a262bf1c0c88ae78092ed13
Donald Zhan <donald.zhan1984@gmail.com>
2015-03-03 12:41:09 +0800

    add test for Person

--- src/Person.php
+++ src/Person.php
@@ -2,21 +2,33 @@
 namespace MudWorld;
 
 use MudWorld\Core\MudObject;
+use MudWorld\Event\WorldDestroyEvent;
+use MudWorld\Event\WorldInitEvent;
 use MudWorld\Event\WorldRunEvent;
 
 class Person extends MudObject
 {
 
-    public static function getSubscribedEvents()
+    public function __construct()
     {
-      return array(
-        'world.run' => array('onWorldRun', 0),
-      );
+
+    }
+
+    public function onWorldDestroy(WorldDestroyEvent $event)
+    {
+        $this->was_destroy = true;
+        return true;
     }
 
     public function onWorldRun(WorldRunEvent $event)
     {
         $this->was_run = true;
+        return true;
+    }
+
+    public function onWorldInit(WorldInitEvent $event)
+    {
+        $this->was_init = true;
         return true;
     }
 }
--- test/PersonTest.php
+++ test/PersonTest.php
@@ -1,18 +1,98 @@
 <?php
 namespace MudWorld\Test;
 
+use MudWorld\Core\WorldInterface;
+use MudWorld\Event\WorldDestroyEvent;
+use MudWorld\Event\WorldInitEvent;
+use MudWorld\Event\WorldRunEvent;
 use MudWorld\Person;
+use MudWorld\World;
 use PHPUnit_Framework_TestCase;
-
 
 class PersonTest extends PHPUnit_Framework_TestCase
 {
 
-  public function testInit()
-  {
+    private function _makePerson()
+    {
+        return new Person();
+    }
 
-    $person = new Person();
+    private function _makeWorld()
+    {
+        return new World();
+    }
 
-    $this->assertInstanceOf('MudWorld\Person', $person);
-  }
+    private function _makeWorldRunEvent(WorldInterface $world)
+    {
+        return new WorldRunEvent($world);
+    }
+
+    private function _makeWorldInitEvent(WorldInterface $world)
+    {
+        return new WorldInitEvent($world);
+    }
+
+    private function _makeWorldDestroyEvent(WorldInterface $world)
+    {
+        return new WorldDestroyEvent($world);
+    }
+
+    public function testInit()
+    {
+
+        $person = $this->_makePerson();
+
+        $this->assertInstanceOf('MudWorld\Person', $person);
+
+    }
+
+    public function testPersonInitReturnTrue()
+    {
+
+        $world = $this->_makeWorld();
+
+        $event = $this->_makeWorldInitEvent($world);
+
+        $person = $this->_makePerson();
+
+        $world->add($person);
+
+        $world->dispatch('world.init', $event);
+        $world->dispatch('world.after.init', $event);
+
+        $this->assertTrue($person->isInit());
+    }
+
+    public function testPersonRunReturnTrue()
+    {
+
+        $world = $this->_makeWorld();
+
+        $event = $this->_makeWorldRunEvent($world);
+
+        $person = $this->_makePerson();
+
+        $world->add($person);
+
+        $world->dispatch('world.run', $event);
+        $world->dispatch('world.after.run', $event);
+
+        $this->assertTrue($person->isRun());
+    }
+
+    public function testPersonDestroyReturnTrue()
+    {
+        $world = $this->_makeWorld();
+
+        $event = $this->_makeWorldDestroyEvent($world);
+
+        $person = $this->_makePerson();
+
+        $world->add($person);
+
+        $world->dispatch('world.destroy', $event);
+        $world->dispatch('world.after.destroy', $event);
+        
+        $this->assertTrue($person->isDestroy());
+    }
 }
